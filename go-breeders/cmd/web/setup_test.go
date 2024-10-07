d56741bfb979a46c2562519bbc13172358e47cd1
hoangtv090103 <hoangtv090103@gmail.com>
2024-10-07 11:51:15 +0700

    Adding a test for the Cat Breeds handler, using our adapter pattern

--- go-breeders/cmd/web/setup_test.go
+++ go-breeders/cmd/web/setup_test.go
@@ -1,7 +1,9 @@
 package main
 
+// This is where we setup our test environment
 import (
 	"go-breeders/configuration"
+	"go-breeders/models"
 	"os"
 	"testing"
 )
@@ -9,11 +11,31 @@ import (
 var testApp application
 
 func TestMain(m *testing.M) {
+    
+    testBackend := &TestBackend{}
+    testAdapter := &RemoteService{
+        Remote: testBackend,
+    }
 
 	// test app
 	testApp = application{
 		App: configuration.New(nil),
+		catService: testAdapter,
 	}
 
 	os.Exit(m.Run())
+}
+
+type TestBackend struct {}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+    breeds := []*models.CatBreed{
+        {
+            ID: 1,
+            Breed: "Tomcat",
+            Details: "Some details",
+        },
+    }
+    
+    return breeds, nil
 }
