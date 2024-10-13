492dd982fa8aecd69f5d88a33ffb00306eed95f1
hoangtv090103 <hoangtv090103@gmail.com>
2024-10-14 06:27:58 +0700

    Filling in the encode() function's logic

--- worker-pool/streamer/streamer.go
+++ worker-pool/streamer/streamer.go
@@ -1,5 +1,12 @@
 package streamer
 
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
 type ProcessingMessage struct {
 	ID         int
 	Successful bool
@@ -52,7 +59,55 @@ func (vd *VideoDispatcher) NewVideo(id int, input, output, encType string, notif
 	}
 }
 
-func (v *Video) encode() {}
+func (v *Video) encode() {
+	var filename string
+
+	switch v.EncodingType {
+	case "mp4":
+		// encode the video
+		name, err := v.encodeToMP4()
+		if err != nil {
+		    // send information to the NotifyChan
+			v.sendToNotifyChan(false, "", fmt.Sprintf("encode failed for %d: %s", v.ID, err.Error()))
+			return
+		}
+
+		filename = fmt.Sprintf("%s.mp4", name)
+	default:
+		v.sendToNotifyChan(false, "", fmt.Sprintf("error processing for %d: invalid encoding type", v.ID))
+	}
+
+	v.sendToNotifyChan(true, filename, fmt.Sprintf("video id %d processed and saved as %s", v.ID, path.Join(v.OutputDir, filename)))
+}
+
+func (v *Video) encodeToMP4() (string, error) {
+	baseFilename := ""
+
+	if !v.Options.RenameOutput {
+		// Get the base filename
+		b := path.Base(v.InputFile)
+		baseFilename = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		// TODO: Generate random file name
+	}
+
+	err := v.Encoder.Engine.EncodeToMP4(v, baseFilename)
+
+	if err != nil {
+		return "", err
+	}
+
+	return baseFilename, nil
+}
+
+func (v *Video) sendToNotifyChan(success bool, filename, message string) {
+	v.NotifyChan <- ProcessingMessage{
+		ID:         v.ID,
+		Successful: success,
+		Message:    message,
+		OutputFile: filename,
+	}
+}
 
 // VideoDispatcher: work pool
 func New(jobQueue chan VideoProccessingJob, maxWorkers int) *VideoDispatcher {
